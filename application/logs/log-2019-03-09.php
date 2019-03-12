b4e769483332ddd274c6d22337f3c3087f61dd48
Ariful <arifsofg@gmail.com>
2019-03-12 13:24:26 +0600

    Design MOdified

---
+++ application/logs/log-2019-03-09.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-03-09 15:47:25 --> Severity: Warning --> require_once(C:\xampp\htdocs\phpdarkpvt\system\core/compat/mbstring.php): failed to open stream: No such file or directory C:\xampp\htdocs\phpdarkpvt\system\core\CodeIgniter.php 287
+ERROR - 2019-03-09 15:47:25 --> Severity: Compile Error --> require_once(): Failed opening required 'C:\xampp\htdocs\phpdarkpvt\system\core/compat/mbstring.php' (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\phpdarkpvt\system\core\CodeIgniter.php 287
+ERROR - 2019-03-09 15:47:26 --> Unable to load the requested class: Session
