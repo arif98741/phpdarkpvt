bab322dae23837ca85eca12aae60d0869af153fb
Ariful Islam <arif98741@gmail.com>
2020-09-29 06:23:18 +0600

    fix path security issue

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-$route['404_override']          = 'error404'; //override by controller
+$route['404_override'] = 'error404'; //override by controller
 $route['default_controller'] = 'front';
-
 
 
 /*
@@ -10,26 +9,26 @@ $route['default_controller'] = 'front';
 ! 	Controller For Admin Profile
 !---------------------------------------------------
 */
-$route['admin']                   = 'admin/admin/index';
-$route['admin/dashboard']           = 'admin/admin/dashboard';
-$route['admin/settings']           = 'admin/admin/settings';
-$route['admin/profile']           = 'admin/admin/profile';
-$route['admin/profile_update']  = 'admin/admin/profile_update';
-$route['admin/settings']          = 'admin/admin/settings';
-$route['admin/logout']               = 'admin/logout'; //for hightlight class list
+$route['admin'] = 'admin/admin/index';
+$route['admin/dashboard'] = 'admin/admin/dashboard';
+$route['admin/settings'] = 'admin/admin/settings';
+$route['admin/profile'] = 'admin/admin/profile';
+$route['admin/profile_update'] = 'admin/admin/profile_update';
+$route['admin/settings'] = 'admin/admin/settings';
+$route['admin/logout'] = 'admin/logout'; //for hightlight class list
 
 /*
 !---------------------------------------------------
 ! 	Front
 !---------------------------------------------------
 */
-$route['blog/view/(:any)/(:num)']         = 'front/view_blog/$1/$2';
-$route['blog/categories']                 = 'front/view_blog/$1/$2';
-$route['blog/category/(:any)/(:num)/page/(:num)']    = 'front/blog_category/$1/$2/$3';
-$route['blog']                            = 'front/blog';
-$route['blog/(:num)']                     = 'front/blog/$1';
-$route['blog/tag/(:any)/(:num)']          = 'front/blog_tag/$1/$2';
-$route['post/view/(:any)/(:num)']         = 'front/post_details/$1/$2';
+$route['blog/view/(:any)/(:num)'] = 'front/view_blog/$1/$2';
+$route['blog/categories'] = 'front/view_blog/$1/$2';
+$route['blog/category/(:any)/(:num)/page/(:num)'] = 'front/blog_category/$1/$2/$3';
+$route['blog'] = 'front/blog';
+$route['blog/(:num)'] = 'front/blog/$1';
+$route['blog/tag/(:any)/(:num)'] = 'front/blog_tag/$1/$2';
+$route['post/view/(:any)/(:num)'] = 'front/post_details/$1/$2';
 
 
 /*
@@ -37,8 +36,8 @@ $route['post/view/(:any)/(:num)']         = 'front/post_details/$1/$2';
 ! 	Main Pages for Front view
 !---------------------------------------------------
 */
-$route['(:any)']            = 'web/frontpage/view_page/$1';
-$route['search_hit']        = 'web/frontpage/search_hit';
-$route['search/(:any)']     = 'web/frontpage/search_result/$1';
+$route['(:any)'] = 'web/frontpage/view_page/$1';
+$route['search_hit'] = 'web/frontpage/search_hit';
+$route['search/(:any)'] = 'web/frontpage/search_result/$1';
 
-$route['translate_uri_dashes']     = FALSE;
+$route['translate_uri_dashes'] = FALSE;
